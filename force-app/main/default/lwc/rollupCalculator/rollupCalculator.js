fc6ba375230e1ce9b58fbd248ef71cd35766a1b5
Scott Fenton <sfenton3@users.noreply.github.com>
2022-07-02 08:04:55 -0400

    Added Rollup Controller
    Added method to generate parent sobjects

--- force-app/main/default/lwc/rollupCalculator/rollupCalculator.js
+++ force-app/main/default/lwc/rollupCalculator/rollupCalculator.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getParentObjects from '@salesforce/apex/RollupCalculator.getParentObjects';
 
-export default class RollupCalculator extends LightningElement {}
+export default class RollupCalculator extends LightningElement {
+    value = 'none';
+    @track results;
+    @track error;
+
+    get statusOptions() {
+        getParentObjects().then(result => {
+            this.results = result;
+            console.log(result);
+        }).catch(error => {
+            this.error = error;
+            console.log(error);
+        })
+        return [
+            {
+                label: 'Account', value: 'Account'
+            },
+            {
+                label: 'Contact', value: 'Contact'
+            }
+        ];
+    }
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+
+}
