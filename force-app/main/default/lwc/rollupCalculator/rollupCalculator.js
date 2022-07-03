a87a841e6843ef4822b4de235d960a7829f08509
Scott Fenton <sfenton3@users.noreply.github.com>
2022-07-03 01:31:59 -0400

    Working on adding support for apex methods
    TODO: Figure out how to bind variable to component

--- force-app/main/default/lwc/rollupCalculator/rollupCalculator.js
+++ force-app/main/default/lwc/rollupCalculator/rollupCalculator.js
@@ -2,30 +2,47 @@ import { LightningElement, track } from 'lwc';
 import getParentObjects from '@salesforce/apex/RollupCalculator.getParentObjects';
 
 export default class RollupCalculator extends LightningElement {
-    value = 'none';
+    valuep = 'none';
+    valuec = 'none';
     @track results;
     @track error;
+    @track parentOptions;
 
-    get statusOptions() {
+    get parentOptions() {
         getParentObjects().then(result => {
             this.results = result;
             console.log(result);
+            console.log(this.normalizeList(result));
+            this.parentOptions = this.normalizeList(result);
         }).catch(error => {
             this.error = error;
             console.log(error);
         })
-        return [
-            {
-                label: 'Account', value: 'Account'
-            },
-            {
-                label: 'Contact', value: 'Contact'
-            }
-        ];
+
+
+    }
+
+    get childOptions() {
+        return { label: 'test', value: 'test' };
     }
 
     handleChange(event) {
-        this.value = event.detail.value;
+        this.valuep = event.detail.value;
+    }
+
+    handleChange2(event) {
+        this.valuec = event.detail.value;
+    }
+
+    normalizeList(val) {
+        let objectList = [];
+
+        for (let i = 0; i < val.length; i++) {
+            const opt = { label: val[i], value: val[i] };
+            objectList.push(opt);
+        }
+
+        return objectList;
     }
 
 }
